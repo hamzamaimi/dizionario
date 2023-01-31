a1aa81c7ea46025726f33bd4466d5d6d7e8d8f15
hamzamaimi <hamzamaimi0901@gmail.com>
2023-01-31 13:35:06 +0100

    feat: added servlet EraseJwt that delete all' cookies

---
+++ src/main/java/servlets/EraseJwt.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="eraseJwt",urlPatterns={"/eraseJwt"})
+public class EraseJwt extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+
+    }
+}
